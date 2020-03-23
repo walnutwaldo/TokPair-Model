fix tf 2.0 bug and fix loss calculation

model/model.py
@@ -85,8 +85,8 @@ def model(inp, target, hidden_size, embedding_size):
 
         #syntax
         syntax_mask, _ = lstm(shifted_target, None, None, hidden_size, 'syntax')
-        syntax_mask = linear(syntax_mask, datasets.num_tokens + 1, tf.exp, 'syntax')
+        syntax_mask = linear(syntax_mask, datasets.num_tokens + 1, None, 'syntax')
 
-        outp = outp - syntax_mask
+        outp = outp - tf.exp(syntax_mask)
 
         return outp, syntax_mask